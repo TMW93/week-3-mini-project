c89b7f78a661011e648684bf5d377ca4e21d303e
TimWong <timwong.tw93@gmail.com>
2024-03-14 22:09:09 +1100

    made stats table, completed userinput check, completed continue checker and made some of the core game function

--- script.js
+++ script.js
@@ -2,44 +2,45 @@ console.log(`Script is working.`);
 
 const possibleInputs = ['R', 'P', 'S'];
 
-// alert("Game Over");
-
 // let gameConfirm = confirm("Are you sure?");
 // console.log(`This is your input: ${gameConfirm}`);
 
-function gameStart() {
-  let userInput = prompt("Rock Paper Scissors");
-  return userInput;
-}
-
 // let myInput = gameStart();
 
-function inputCheck() {
-
-  return;
+//Game stats
+let gameStats = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+  moveHistory: {
+    rock: 0,
+    paper: 0,
+    scissors: 0
+  }
 }
 
-function statTracker() {
-  let gameStats = {
-    wins: 0,
-    losses: 0,
-    ties: 0,
-    moveHistory: {
-      rock: 0,
-      paper: 0,
-      scissors: 0
-    }
-  };
-
+//Checks user input and updates game stats if input is valid
+function inputCheck(user) {
+  if(user === possibleInputs[0]) {
+    gameStats.moveHistory.rock++;
+  } else if(user === possibleInputs[1]) {
+    gameStats.moveHistory.paper++;
+  } else if(user === possibleInputs[2]) {
+    gameStats.moveHistory.scissors++;
+  } else {
+    alert("Invalid input. Please input R, P or S.");
+  }
 }
 
 function aiTurn() {
 }
 
-function winCheck() {
+//Compares user and AI input to determine result
+function winCheck(user, ai) {
+  let result;
   if(user === possibleInputs[0] && ai === possibleInputs[0]) {
     //Rock Tie 
-  } else if (user === possibleInputs[1] &&* ai === possibleInputs[1] {
+  } else if (user === possibleInputs[1] && ai === possibleInputs[1]) {
     //Paper Tie
   } else if(user === possibleInputs[2] && ai === possibleInputs[2]) {
     //Scissors Tie
@@ -56,13 +57,49 @@ function winCheck() {
   } else if(user === possibleInputs[1] && ai === possibleInputs[2]) {
     //AI Scissors Win
   }
-)
+  return result;
+}
 
-function continue() {
-  let userContinue = confirm("Do you want to keep playing?");
-  if(!userContinue){
+function gameContinue() {
+  let game = true;
+  //RPS will run until the function returns a false boolean
+  if(game) {
+    let userContinue = confirm("Do you want to keep playing?");
+    if(!userContinue){
+      game = false;
+    }
+  }
+  return game;
+}
 
+//Game Runs
+function gameStart() {
+  let gameRun = gameContinue()
+  let turnCounter = 1;
+  while(gameRun) {
+    //Take Input
+    let userInput = prompt("Rock Paper Scissors");
+    //Check Input
+
+    //Store Input and turn counter if valid
+
+    //Run AIs turn
+
+    //Check Results
+
+    //Store Results
+
+    //Check if user wants to continue
+    if(!gameRun) {
+      break;
+    } else {
+      //Increase turn counter
+      turnCounter++;
+    }
   }
 }
+
+//After game stops stat screen will show
+
 
 console.log(`This is your input: ${myInput}`);
