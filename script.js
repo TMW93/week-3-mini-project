d416421c7f30e3a28d8976b628f7df58fc181976
TimWong <timwong.tw93@gmail.com>
2024-03-14 21:07:23 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log(`Script is working.`);
+
+
+
+// alert("Game Over");
+
+// let gameConfirm = confirm("Are you sure?");
+// console.log(`This is your input: ${gameConfirm}`);
+
+function gameStart() {
+  let userInput = prompt("Rock Paper Scissors");
+  return userInput;
+}
+
+// let myInput = gameStart();
+
+console.log(`This is your input: ${myInput}`);
